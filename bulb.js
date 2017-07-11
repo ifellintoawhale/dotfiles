a3857c6a9ca865b37f5faeb202a9d303f86a4601
ifellintoawhale <jason@ifellintoawhale.com>
2017-07-11 04:25:08 -0500

    Add files via upload
    .

---
+++ bulb.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#201e20", "#c373b41", "#dc8249", "#e1f14b", "#a54242", "#a54242", "#de935f", "#f0c674", "#993a46", "#e76474", "#712828", "#c04949", "#b87e40", "#f6dabc", "#707880", "#c5c8c6"]);
+t.prefs_.set('foreground-color', "#c5c8c6");
+t.prefs_.set('background-color', "#201e20");
+t.prefs_.set('cursor-color', "c5c8c6");
