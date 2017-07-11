1d6a6463b4c44b736d4b62c1c4b1325e938cd99d
ifellintoawhale <jason@ifellintoawhale.com>
2017-07-11 04:45:59 -0500

    Update bulb.js

--- bulb.js
+++ bulb.js
@@ -1,4 +1,4 @@
-t.prefs_.set("color-palette-overrides": ["#201e20","#dc8249","#a54242","#de935f","#993a46","#712828","#b87e40","#707880","#373b41","#e1f14b","#cc6666","#f0c674","#e76474","#c04949","#f6dabc","#c5c8c6"]);
+t.prefs_.set('color-palette-overrides',["#201e20", "#c373b41", "#dc8249", "#e1f14b", "#a54242", "#a54242", "#de935f", "#f0c674", "#993a46", "#e76474", "#712828", "#c04949", "#b87e40", "#f6dabc", "#707880", "#c5c8c6"]);
 t.prefs_.set('foreground-color', "#c5c8c6");
 t.prefs_.set('background-color', "#201e20");
 t.prefs_.set('cursor-color', "c5c8c6");
